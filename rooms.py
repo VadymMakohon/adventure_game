68ba9f3a0cb37447c2591e6a6e26e0360c80404d
VadymMakohon <errorks7@gmail.com>
2024-07-12 14:35:17 +0200

    Allow NPCs and items to be placed in rooms

--- rooms.py
+++ rooms.py
@@ -1,40 +1,26 @@
 class Room:
-    def __init__(self, name, description, exits, locked=False, puzzle=None, npc=None):
+    def __init__(self, name, description, exits, items=None, npcs=None):
         self.name = name
         self.description = description
         self.exits = exits
-        self.items = []
-        self.locked = locked
-        self.puzzle = puzzle
-        self.npc = npc
+        self.items = items or {}
+        self.npcs = npcs or {}
 
     def add_item(self, item):
-        self.items.append(item)
+        self.items[item.name] = item
 
     def take_item(self, item_name):
-        for item in self.items:
-            if item.name == item_name:
-                self.items.remove(item)
-                return item
-        return None
+        return self.items.pop(item_name, None)
 
-    def show_items(self):
-        if self.items:
-            print("You see the following items:")
-            for item in self.items:
-                print(f" - {item.name}")
-        else:
-            print("There are no items here.")
+    def add_npc(self, npc):
+        self.npcs[npc.name] = npc
 
-    def solve_puzzle(self, answer):
-        if self.puzzle and answer == self.puzzle['answer']:
-            self.locked = False
-            print("You solved the puzzle! The door is now unlocked.")
-        else:
-            print("That's not correct.")
-    
-    def interact_with_npc(self):
-        if self.npc:
-            self.npc.talk()
-        else:
-            print("There is no one here to talk to.")
+    def get_npc(self, npc_name):
+        return self.npcs.get(npc_name)
+
+    def describe(self):
+        print(self.description)
+        for npc in self.npcs.values():
+            print(f"You see {npc.name}.")
+        for item in self.items.values():
+            print(f"There is a {item.name} here.")
