05e8f009c541499f801ba1be8a11a9d4805a0603
VadymMakohon <errorks7@gmail.com>
2024-07-12 14:32:58 +0200

    Define item behaviors

--- items.py
+++ items.py
@@ -2,3 +2,12 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+class HealingPotion(Item):
+    def __init__(self):
+        super().__init__("Healing Potion", "Heals you for 20 health points.")
+
+    def use(self, player):
+        player.heal(20)
+        player.remove_item(self.name)
+        print("You used a Healing Potion.")
