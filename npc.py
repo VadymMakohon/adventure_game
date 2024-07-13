806ea9dd85f57bf59c87ddb4e1679a9b9089b943
VadymMakohon <errorks7@gmail.com>
2024-07-13 03:22:30 +0200

    updated npc file

--- npc.py
+++ npc.py
@@ -1,8 +1,7 @@
 class NPC:
-    def __init__(self, name, description, dialog):
+    def __init__(self, name, dialog):
         self.name = name
-        self.description = description
         self.dialog = dialog
 
     def talk(self):
-        print(f"{self.name} says: {self.dialog}")
+        return self.dialogue
